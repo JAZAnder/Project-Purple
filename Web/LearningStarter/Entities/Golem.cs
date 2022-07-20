67aaa809163a14d1335070c03dc9f5f8ee1ff2ed
Joshua Cantu <joshua.cantu-2@selu.edu>
2022-07-20 15:48:20 -0700

    Configured Golem.cs

--- Web/LearningStarter/Entities/Golem.cs
+++ Web/LearningStarter/Entities/Golem.cs
@@ -1,13 +1,24 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.Text.Json.Serialization;
 namespace LearningStarter.Entities
 {
     public class Golem
     {
         public int Id { get; set; }
+        public string Henry { get; set; }
 
-        public int Health { get; set; }
+        public List<GolemSpell> GolemSpells { get; set; }
+            = new List<GolemSpell>(3);
 
-        public Spell[] Spells { get; set; }
+
+        public int GolemTypeId { get; set; }
+        public GolemType GolemType { get; set; }
+
+
+        public int? TargetGolemId { get; set; }
+        public Golem TargetGolem { get; set; }
+
+        
     }
 }
