0d52a53b5de05706668d4dab20ae9b7e9c037de6
Joshua Cantu <joshua.cantu-2@selu.edu>
2022-07-20 15:47:46 -0700

    Created and Configured Battle.cs

---
+++ Web/LearningStarter/Entities/Battle.cs
@@ -0,0 +1,20 @@
+﻿namespace LearningStarter.Entities
+{
+    public class Battle
+    {
+        public int Id { get; set; }
+        public int PlayerOneId { get; set; }
+        public User PlayerOne { get; set; }
+
+        public int PlayerOneGolemId { get; set; }
+        public Golem PlayerOneGolem { get; set; }
+        public int PlayerOneGolemHealth {get; set; }
+
+        public int PlayerTwoId { get; set; }
+        public User PlayerTwo { get; set; }
+
+        public int PlayerTwoGolemId { get; set; }
+        public Golem PlayerTwoGolem { get; set; }
+        public int PlayerTwoGolemHealth { get; set; }
+    }
+}
