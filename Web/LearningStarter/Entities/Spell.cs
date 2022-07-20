95c3d1583f65a7d5c8b0359ed7ce957ee3ab804e
Joshua Cantu <joshua.cantu-2@selu.edu>
2022-07-20 15:48:52 -0700

    Configured Spells

--- Web/LearningStarter/Entities/Spell.cs
+++ Web/LearningStarter/Entities/Spell.cs
@@ -3,5 +3,13 @@
     public class Spell
     {
         public int Id { get; set; }
+        public string SpellName { get; set; }
+        public string Description { get; set; }
+
+        public int ChanceId { get; set; }
+        public Chance Chance { get; set; }
+
+        public int DamageDealt { get; set; }
+        public int DamageRecived { get; set; }
     }
 }
