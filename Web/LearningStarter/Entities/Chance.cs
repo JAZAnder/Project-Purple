a76fd65fe347cc4051041b8526d3138f04bb9267
Joshua Cantu <joshua.cantu-2@selu.edu>
2022-07-20 15:48:05 -0700

    Created and Staged Chance.cs

---
+++ Web/LearningStarter/Entities/Chance.cs
@@ -0,0 +1,9 @@
+﻿namespace LearningStarter.Entities
+{
+    public class Chance
+    {
+        public int Id { get; set; }
+        public string ChanceToHit { get; set; }
+        public int ChanceLevel { get; set; }
+    }
+}
