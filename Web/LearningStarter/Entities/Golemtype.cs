c4467ea4284cd4b7c119d13bd5da98e842b0d758
Joshua Cantu <joshua.cantu-2@selu.edu>
2022-07-20 15:48:35 -0700

    Configuered GolemTypes

--- Web/LearningStarter/Entities/Golemtype.cs
+++ Web/LearningStarter/Entities/Golemtype.cs
@@ -1,6 +1,15 @@
-﻿namespace LearningStarter.Entities
+﻿using System.Collections.Generic;
+
+namespace LearningStarter.Entities
 {
     public class GolemType
     {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public int Maxhealth { get; set; }
+        
+        public List<GolemSpell> GolemSpells { get; set; }
+            = new List<GolemSpell>();
     }
 }
