3cc274900f28300f46e7ef388d0680e2f05be2fc
Joshua Cantu <joshua.cantu-2@selu.edu>
2022-07-20 15:35:17 -0700

    Configured GolemSpells

--- Web/LearningStarter/Entities/GolemSpell.cs
+++ Web/LearningStarter/Entities/GolemSpell.cs
@@ -3,5 +3,10 @@
     public class GolemSpell
     {
         public int Id {get; set;}
+        public int SpellId {get; set;}
+        public Spell Spell {get; set;}
+
+        public int GolemId {get; set;}
+        public Golem Golem {get; set;}
      }
 }
