83e6230279d64e9c41ea50fa6a1149acd225a6d6
Joshua Cantu <joshua.cantu-2@selu.edu>
2022-07-25 10:29:35 -0700

    Ctreated A place to seed Golem Types

---
+++ Web/LearningStarter/Seeds/SeededGolemTypes.cs
@@ -0,0 +1,14 @@
+﻿using LearningStarter.Entities;
+namespace LearningStarter.Seeds
+
+{
+    public static class SeededGolemTypes
+    {
+        public static GolemType Bob = new GolemType {
+            Type = "Fire"
+            , Description = "High TEmpored High Power, Low Health"
+            , Maxhealth = 50
+            
+        };
+    }
+}
