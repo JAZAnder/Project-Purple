58a1b7b16f60b05338372e1274f491c5f1a86587
Joshua Cantu <joshua.cantu-2@selu.edu>
2022-07-25 10:28:00 -0700

    Added All Types to Context

--- Web/LearningStarter/Data/DataContext.cs
+++ Web/LearningStarter/Data/DataContext.cs
@@ -11,6 +11,12 @@ namespace LearningStarter.Data
         }
 
         public DbSet<User> Users { get; set; }
+        public DbSet<Battle> battles { get; set; }
+        public DbSet<Chance> chances { get; set; }
+        public DbSet<Golem> golems { get; set; }
+        public DbSet<GolemSpell> golemSpells { get; set; }
+        public DbSet<GolemType> golemTypes { get; set; }
+        public DbSet<Spell> spells { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
